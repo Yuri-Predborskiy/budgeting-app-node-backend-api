23a890381802bd985639b1bb415017c9121f241a
Yuri Predborski <shini1984@gmail.com>
2021-11-02 14:29:40 +0100

    feat: expand expense to add category and description

---
+++ db/models/category.model.js
@@ -0,0 +1,23 @@
+const { DataTypes} = require('sequelize');
+const sequelize = require('../db');
+const Expense = require('./expense.model');
+
+const Category = sequelize.define('category', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: DataTypes.STRING,
+  // icon?
+});
+
+// todo: check if options are necessary
+Category.belongsTo(Expense);
+
+module.exports = Category;
--- db/models/expense.model.js
+++ db/models/expense.model.js
@@ -1,14 +1,15 @@
-const { DataTypes} = require('sequelize');
+const { DataTypes } = require('sequelize');
 
 const sequelize = require('../db');
 const Account = require('./account.model');
+const Category = require('./category.model');
 
 const ExpenseModel = sequelize.define('expense', {
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     autoIncrement: true,
-    primaryKey: true,
+    primaryKey: true
   },
   date: {
     type: DataTypes.DATE,
@@ -19,18 +20,31 @@ const ExpenseModel = sequelize.define('expense', {
     allowNull: false,
     references: {
       model: Account,
-      key: 'id',
+      key: 'id'
     }
   },
   amount: {
     type: DataTypes.FLOAT,
     allowNull: false,
   },
+  description: DataTypes.STRING,
+  categoryId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+    references: {
+      model: Category,
+      key: 'id'
+    }
+  }
 });
 
 ExpenseModel.belongsTo(Account, {
   onDelete: 'CASCADE',
   allowNull: false
 });
+ExpenseModel.hasOne(Category, {
+  onDelete: 'RESTRICT',
+  allowNull: false
+})
 
 module.exports = ExpenseModel;
