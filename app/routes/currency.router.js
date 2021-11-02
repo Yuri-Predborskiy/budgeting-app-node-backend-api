e16252e3d7dcf018ec900a2ede670b1a4a0b9e1f
Yuri Predborski <shini1984@gmail.com>
2021-11-02 13:24:01 +0100

    feat: create currency router/controller combo

---
+++ app/routes/currency.router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const currencyService = require('../services/currency.service');
+
+router.get('/', currencyService.getAll);
+
+router.get('/:code', currencyService.getByCode);
+
+// todo: add validator
+router.post('/', currencyService.create);
+
+// todo: add validator
+router.patch('/:code', currencyService.updateByCode);
+
+// todo: add validator
+router.delete('/:code', currencyService.deleteByCode);
+
+module.exports = router;
