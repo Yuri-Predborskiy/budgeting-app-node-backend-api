d08e5bba10fe6d6a89741c9db8768abd7be2eba7
Yuri Predborski <shini1984@gmail.com>
2022-03-05 14:24:26 +0100

    feat: add migrations for income and income category

---
+++ src/db/migrations/20220305134922-create-income-category-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'income-categories', {
+        name: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        deletedAt: {
+          type: Sequelize.DATE,
+        }
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('income-category');
+  }
+};
---
+++ src/db/migrations/20220305135121-create-income-table.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'incomes', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        date: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        accountId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+          references: {
+            model: 'accounts',
+            key: 'id',
+            as: 'accountId',
+          },
+        },
+        amount: {
+          allowNull: false,
+          type: Sequelize.FLOAT
+        },
+        category: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          references: {
+            model: 'categories',
+            key: 'name'
+          },
+          onUpdate: 'CASCADE',
+          onDelete:  'RESTRICT',
+        },
+        description: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        deletedAt: {
+          type: Sequelize.DATE,
+        }
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('expenses');
+  }
+};
